df53e9a4410487f403f3bf8fc5c1c17b035c66c4
Kent C. Dodds <me+github@kentcdodds.com>
2022-11-10 15:31:44 -0700

    only deploy migrations in primary

---
+++ other/prestart.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const { spawn } = require("child_process");
+
+async function go() {
+  try {
+    const primary = await fs.promises.readFile("/litefs/data/.primary", "utf8");
+    process.env.PRIMARY_REGION = primary.trim();
+  } catch (error) {
+    process.env.PRIMARY_REGION = process.env.FLY_REGION;
+  }
+
+  if (process.env.PRIMARY_REGION === process.env.FLY_REGION) {
+    await deployMigrations();
+  }
+}
+go();
+
+async function deployMigrations() {
+  const command = "npx prisma migrate deploy";
+  const child = spawn(command, { shell: true, stdio: "inherit" });
+  await new Promise((res, rej) => {
+    child.on("exit", (code) => {
+      if (code === 0) {
+        res();
+      } else {
+        rej();
+      }
+    });
+  });
+}
--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
-# npx prisma migrate deploy
+node other/prestart.js
 npm run start
